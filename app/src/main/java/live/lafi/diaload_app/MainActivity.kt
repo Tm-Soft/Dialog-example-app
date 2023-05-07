158181eb9d2cea9a283d0deb24685bbb8dbf3f80
bbqck <kukwonho@gmail.com>
2023-05-07 12:52:21 +0900

    [activity_main.xml] One, Two, String Dialog Button 추가
    [gradle.xml]
    viewBinding True 옵션 추가 및 동기화
    
    [MainActivity.kt]
    1. binding 객체 생성 및 setContentView 적용
    
    2. Dialog 객체 생성 및 각 버튼에 클릭 이벤트 생성

--- app/src/main/java/live/lafi/diaload_app/MainActivity.kt
+++ app/src/main/java/live/lafi/diaload_app/MainActivity.kt
@@ -2,13 +2,55 @@ package live.lafi.diaload_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import live.lafi.diaload_app.databinding.ActivityMainBinding
 import live.lafi.library_dialog.Dialog
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        Dialog.with(this).title("테스트 화면").showOneButtonDialog()
+
+
+        // Dialog 객체 생성
+        val dialogObj = Dialog
+                            .with(this)
+                            .title("Dialog Default Title")
+                            .content("Default Content")
+                            .positiveText("OK")
+                            .negativeText("Cancel")
+
+        binding.btnOneButtonDialog.setOnClickListener {
+            dialogObj
+                .positiveListener {
+                    Toast.makeText(this, "Positive Button Click", Toast.LENGTH_LONG).show()
+                }
+                .showOneButtonDialog()
+        }
+
+        binding.btnTwoButtonDialog.setOnClickListener {
+            dialogObj
+                .positiveListener {
+                    Toast.makeText(this, "Positive Button Click", Toast.LENGTH_LONG).show()
+                }
+                .negativeListener {
+                    Toast.makeText(this, "Negative Button Click", Toast.LENGTH_LONG).show()
+                }
+                .showTwoButtonDialog()
+        }
+
+        binding.btnInputStringButtonDialog.setOnClickListener {
+            dialogObj
+                .stringCallbackListener { text ->
+                    Toast.makeText(this, "Input String : $text", Toast.LENGTH_LONG).show()
+                }
+                .negativeListener {
+                    Toast.makeText(this, "Negative Button Click", Toast.LENGTH_LONG).show()
+                }
+                .showEditTextDialog()
+        }
     }
 }
